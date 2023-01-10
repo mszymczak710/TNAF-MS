03f981fe93738cab53876c82ec76e61063e34695
mszymczak710 <mszymczak710@o2.pl>
2023-01-10 11:11:56 +0100

    Zadanie3 i poprzednie zadania

---
+++ Zajecia5-React/my-app/src/components/Users/Users.js
@@ -0,0 +1,16 @@
+const people = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+  },
+  {
+    firstName: "Bob",
+    lastName: "Smith",
+  }
+]
+
+export default people;
